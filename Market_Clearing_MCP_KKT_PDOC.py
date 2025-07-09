fd3e18d1952f6384c6d88b536449a27d751d5327
João Augusto Silva Lêdo <joao.ledo@unesp.br>
2025-07-09 15:03:07 -0300

    Update Market_Clearing_MCP_KKT_PDOC.py

--- Market_Clearing_MCP_KKT_PDOC.py
+++ Market_Clearing_MCP_KKT_PDOC.py
@@ -1,4 +1,3 @@
-
 #******************************************************************************************************************************************************
 #*                                                             MARKET CLEARING MCP MODEL                                                              *
 #*************************************************************************************************-Developed by Joao Augusto Silva Ledo-***************
@@ -64,22 +63,11 @@ class ReturnOutputSolution:  # method of the class accountable for creating its 
     def __str__(self):  # method of the class accountable for returning all its atributes in a dynamic string
         return str(self.__dict__)
 
-def get_values_from_user(prompt):
+def get_values_from_user(prompt, allowed_values):
     while True:
         try:
             value = int(input(prompt))
-            if (value != 1) and (value != 2):
-                print("\n PLEASE SELECT CARREFULLY! \n")
-            else:
-                return value
-        except ValueError:
-            print("\n Invalid input! {prompt}")
-            
-def get_values_from_user_Solvers(prompt):
-    while True:
-        try:
-            value = int(input(prompt))
-            if (value > 7) or (value <= 0):
+            if value not in allowed_values:
                 print("\n PLEASE SELECT CARREFULLY! \n")
             else:
                 return value
@@ -87,12 +75,12 @@ def get_values_from_user_Solvers(prompt):
             print("\n Invalid input! {prompt}")
             
 def Linear_or_Non_Linear():
-        Selec_Linear_Non_Linear = get_values_from_user("\n Please select: \n 1 - Linear Solvers \n 2 - Non-linear Solver \n Type the value: ")
+        Selec_Linear_Non_Linear = get_values_from_user("\n Please select: \n 1 - Linear Solvers \n 2 - Non-linear Solver \n Type the value: ", list(range(1, 3)))
         if Selec_Linear_Non_Linear == 1:
-            Select_Solver = get_values_from_user_Solvers("\n Please select: \n 1 - glpk \n 2 - cbc \n 3 - highs \n 4 - cplex \n 5 - gurobi \n 6 - xpress \n 7 - scip \n Type the value: ")
+            Select_Solver = get_values_from_user("\n Please select: \n 1 - glpk \n 2 - cbc \n 3 - highs \n 4 - cplex \n 5 - gurobi \n 6 - xpress \n 7 - scip \n Type the value: ", list(range(1, 8)))
             solver_name = LinearSolver[Select_Solver-1]
         else:
-            Select_Solver = get_values_from_user_Solvers("\n Please select: \n 1 - ipopt \n 2 - knitro \n 3 - conopt \n 4 - bonmin \n 5 - couenne \n 6 - baron \n 7 - snopt \n Type the value: ")
+            Select_Solver = get_values_from_user("\n Please select: \n 1 - ipopt \n 2 - knitro \n 3 - conopt \n 4 - bonmin \n 5 - couenne \n 6 - baron \n 7 - snopt \n Type the value: ", list(range(1, 8)))
             solver_name = Nonlinear[Select_Solver-1]
         return [Select_Solver, solver_name]
 
@@ -211,7 +199,7 @@ def positivity_d_max_constraint(model, j, c):
     return model.mu_d_max[j, c] >= 0
 MarketClearingModel.positivity_d_max_constraint = Constraint(MarketClearingModel.JC, rule=positivity_d_max_constraint)
 
-MCP_KKT_or_PDOC = get_values_from_user("\n Please select: \n 1 - KKT MCP Market Clearing model \n 2 - PDOC MCP Market Clearing model \n Type the value: ")
+MCP_KKT_or_PDOC = get_values_from_user("\n Please select: \n 1 - KKT MCP Market Clearing model \n 2 - PDOC MCP Market Clearing model \n Type the value: ", list(range(1, 3)))
 
 if MCP_KKT_or_PDOC == 1:
     MCP = 'KKT MCP'
@@ -240,7 +228,7 @@ else:
 # Add suffix for duals
 MarketClearingModel.dual = Suffix(direction=Suffix.IMPORT)
     
-Select_Solve_Locally_NEOS = get_values_from_user("\n Please select: \n 1 - Solve Locally \n 2 - Solve using NEOS Server \n Type the value: ")
+Select_Solve_Locally_NEOS = get_values_from_user("\n Please select: \n 1 - Solve Locally \n 2 - Solve using NEOS Server \n Type the value: ", list(range(1, 3)))
 
 if Select_Solve_Locally_NEOS == 1:
     (Select_Solver, solver_name) = Linear_or_Non_Linear()
